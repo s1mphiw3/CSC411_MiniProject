966d635b394178269c2b4047bc925f337f2c0d2a
s1mphiw3 <95270598+s1mphiw3@users.noreply.github.com>
2023-07-30 07:57:01 +0200

    socket programming first upload

---
+++ Socket Programming_Client & Server/client.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 06:40:03 2022
+
+@author: Manyatsi
+"""
+
+import socket
+from threading import Thread
+# if the server is not on this machine,
+# put the private (network) IP address (e.g 192.168.1.2)
+host = "localhost"
+port =  8080# server's port
+# initialize TCP socket
+connector_sockect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Connecting to " +str(host) + ": " +str(port)+"...")
+# connect to the server
+connector_sockect.connect((host,port))
+print("[+] Connected.")
+# prompt the client for a name
+print("Enter your name :")
+name = input()
+def messages():
+    while True:
+        message = connector_sockect.recv(1024).decode()
+        print("\t \n" + message)
+thread = Thread(target=messages) # make a thread that listens for messages to this client
+thread.daemon = True # make the thread daemon so it ends whenever the main thread ends
+thread.start()
+print("Start sending messages, enter x to stop sending")
+while True:
+    message = input() # input message we want to send to the server
+    
+    if message == 'x': # exit program if x is typed
+        break
+    message =str(name) + " : " + str(message)
+    connector_sockect.send(message.encode()) #send the message
+    
+# close the socket
+connector_sockect.close()
---
+++ Socket Programming_Client & Server/server.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 06:16:30 2022
+
+@author: Manyatsi
+"""
+import socket
+from threading import Thread
+
+host = "localhost"
+port = 8080 # port we want to use
+client_sockets = set() # create list of connected clients
+connector_sockect = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a TCP socket
+connector_sockect.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # ensure port can be re-used
+connector_sockect.bind((socket.gethostname(), port))# bind socket to address
+connector_sockect.listen()# listen for upcoming connections
+print( "Waiting for clients...")
+def clients(client):
+    while True:
+        try:
+            msg = client.recv(1024).decode() #listen for messages from client
+        except Exception as e:
+                client_sockets.remove(client) #remove clients that are no longer connected
+        else: #print mmessage that we receive
+            print(str(msg))
+        
+        for client_socket in client_sockets: # go through all connected sockets and send message
+            client_socket.send(msg.encode())  
+    
+while True:
+    c_socket,c_address = connector_sockect.accept()
+    print(str(c_address) + "connected")
+    client_sockets.add(c_socket) #add new client to the list of sockets
+    thread = Thread(target=clients,args=(c_socket,)) #start a new thread that listens for each clients message
+    thread.daemon = True  #make a thread daemon so it ends when main thread ends
+    thread.start() #start thread
+    
+#close client sockets
+for client in client_sockets:
+    client.close()
+ #close server socket
+connector_sockect.close()
+    
